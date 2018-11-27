8a1cc73ffc8c1e1bade76d9b8bd6c5747d3040c3
Inf1mum <vadim.lazuk@mail.ru>
2018-11-27 23:29:07 +0300

    commit by Vadzim

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-var start = Vue.extend({
-  template: '#start',
-   data: function () {
-      return {name: 'Hello'}
-    },
-});
+
+
+var start = Vue.extend({  template: '#start'});
+var lesson1 = Vue.extend({  template: '#lesson1'});
+var lesson2 = Vue.extend({  template: '#lesson2'});
+var lesson3 = Vue.extend({  template: '#lesson3'});
 
 var router = new VueRouter({
   routes: [
-    {path: '/', component: start}
+    {path: '/', component: start},
+    {path: '/lesson/1', component: lesson1},
+    {path: '/lesson/2', component: lesson2},
+    {path: '/lesson/3', component: lesson3}
+
 
 ]});
 
