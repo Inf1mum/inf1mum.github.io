7e078d28519fba97bcc80461b4bd11c2b978b999
Inf1mum <vadim.lazuk@mail.ru>
2021-07-27 17:28:27 +0300

    add lesson 3

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ var start = Vue.extend({  template: '#start'});
 var lesson1 = Vue.extend({  template: '#lesson1'});
 var lesson2 = Vue.extend({  template: '#lesson2'});
 var lesson3 = Vue.extend({  template: '#lesson3'});
+var lesson4 = Vue.extend({  template: '#lesson4'});
+
 
 var router = new VueRouter({
   routes: [
     {path: '/', component: start},
     {path: '/lesson/1', component: lesson1},
     {path: '/lesson/2', component: lesson2},
-    {path: '/lesson/3', component: lesson3}
-
-
-]});
+    {path: '/lesson/3', component: lesson3},
+    {path: '/lesson/4', component: lesson4}
+  ]});
 
 
 new Vue({
