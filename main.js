2c4bc170784fff3f119cc1cf0e9f46d8255a1952
Vadzim Lazuk <vadzim.lazuk2@ibm.com>
2021-08-26 22:34:59 +0300

    add tasks for section 5

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var lesson1 = Vue.extend({  template: '#lesson1'});
 var lesson2 = Vue.extend({  template: '#lesson2'});
 var lesson3 = Vue.extend({  template: '#lesson3'});
 var lesson4 = Vue.extend({  template: '#lesson4'});
+var lesson5 = Vue.extend({  template: '#lesson5'});
+
 
 
 var router = new VueRouter({
@@ -13,14 +15,14 @@ var router = new VueRouter({
     {path: '/lesson/1', component: lesson1},
     {path: '/lesson/2', component: lesson2},
     {path: '/lesson/3', component: lesson3},
-    {path: '/lesson/4', component: lesson4}
+    {path: '/lesson/4', component: lesson4},
+    {path: '/lesson/5', component: lesson5}
   ]});
 
 
 new Vue({
   el: '#app',
   router: router
-
 });
 
 
